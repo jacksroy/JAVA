91ec305e83ecb071310da176e9bf23d4ed0f50b3
jacksroy <sevay1824@gmail.com>
2025-09-13 19:59:46 +0530

    Create Varabiles.java
    Varabiles in java

---
+++ Varabiles.java
@@ -0,0 +1,25 @@
+class Varabiles{
+    public static void main(String a[]){ 
+        int num=3;//lower case
+        int num2=17;//lower case and digits
+        int result=num+num2;
+        int Num=1;//uoercase
+        int numberValue=10;//camel case
+        int _hello=29;//under score varaibles
+        int $hello1=56;//Dolloar sign variables
+        System.out.println(Num);
+        System.out.println(numberValue);
+        System.out.println(_hello);
+        System.out.println($hello1);
+        System.out.println("Result is:"+ result);
+    }
+}
+
+//output
+//  javac Varabiles.java
+// java Varabiles
+// 1
+// 10
+// 29
+// 56
+// Result is:20
