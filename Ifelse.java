960a0c6d9b8d346406ea7ce696f182e4191584c1
jacksroy <sevay1824@gmail.com>
2025-09-14 21:41:43 +0530

    Create Ifelse.java
    If Else

---
+++ Ifelse.java
@@ -0,0 +1,17 @@
+public class Ifelse {
+    public static void main(String args[]){
+        int age=18;
+        if(age>=18){
+        System.out.println("Age is:"+age);//here multiple statements so we use flower vraces to if condtion 
+        System.out.println("can Watch Movie");
+    }
+
+        else
+        System.out.println("Can't watch this movie");//here we are just printing a single statement so we did noy use flower braces we can also use it but in case of we are using multiple staements we want to use flower braces
+    }
+}
+//
+// PS C:\REWORK\Java> javac Ifelse.java
+// PS C:\REWORK\Java> java Ifelse      
+// Age is:18
+// can Watch Movie
