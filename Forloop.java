d6611557ff5ad06e0fa6c2f6dc82d349bb4b466c
jacksroy <sevay1824@gmail.com>
2025-09-19 20:06:08 +0530

    Create Forloop.java

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+
+public class Forloop {
+
+    public static void main(String args[]){
+    
+        int i=10;
+        for(;i<=20;) { // for(int i= 10 ;i<=20 ;i++)
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
