c217f2f68621cef8ccedf164b7e0c31fbfaa01f0
jacksroy <sevay1824@gmail.com>
2025-10-14 19:24:32 +0530

    Create Static_Block.java

---
+++ Static_Block.java
@@ -0,0 +1,29 @@
+class Block{
+    String name;
+    int age;
+    void show(){
+        System.out.println("Name: "+name+" Age: "+age);
+    }
+     static{
+        // this is static block call only once in the program 
+        System.out.println("This is static Block");
+    }
+}
+
+public class Static_Block {
+    public static void main(String[] args) throws ClassNotFoundException {
+        // Block obj =new Block();
+        // obj.name="Vijayamma";
+        // obj.age=39;
+
+        // obj.show();
+        Class.forName("Block");// we need to create object or constructor to call static block or the alternative way is this by calling by Class.forName("Class_Name")
+        
+    }
+}
+
+
+// output
+// PS C:\REWORK\Java\Static Keyword> javac Static_Block.java
+// PS C:\REWORK\Java\Static Keyword> java Static_Block
+// This is static Block
