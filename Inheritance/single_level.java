1367f52b2992a904fcf57b8c07df9964ae449944
jacksroy <sevay1824@gmail.com>
2025-11-12 19:31:20 +0530

    single_level.java
    single level inheritance

---
+++ Inheritance/single_level.java
@@ -0,0 +1,36 @@
+
+class A{  
+// parent class or super class or base class
+    public int add(int a,int b){
+
+        return a+b;
+
+    }
+}
+
+class B extends A{
+    // child class or sub class or  derived class  
+    //this is a single level inheritance
+    public int sub(int a ,int b){
+        return a-b;
+    }
+}
+
+
+public class single {
+    public static void main(String[] args) {
+        B obj= new B();// which is last class we need to create that class obj so that we can take all the properties  
+
+        System.out.println("Sum of this Value : "+obj.add(10, 7));
+        System.out.println("Different of this Value : "+obj.sub(10, 7));
+        
+    }
+}
+
+
+
+//output
+// PS C:\REWORK\Java\Inheritence> javac single.java
+// PS C:\REWORK\Java\Inheritence> java single
+// Sum of this Value : 17
+// Different of this Value : 3
